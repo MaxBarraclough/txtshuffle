58f977608666e6ee5137eef78da9423d4f03299c
Max Barraclough <maxbarraclough@users.noreply.github.com>
2017-10-28 18:31:33 +0100

    Move code out of constructor, into 'start' method
    to get it to run on the right thread

--- src/main/java/engineer/maxbarraclough/txtshuffle/gui/MainApp.java
+++ src/main/java/engineer/maxbarraclough/txtshuffle/gui/MainApp.java
@@ -11,33 +11,27 @@ import javafx.stage.Stage;
 
 public final class MainApp extends Application {
 
-    protected final Scene rootScene;
+    protected /*final*/ Scene rootScene;
     
     /**
      * selectDataSourceScene
      */
-    protected final Scene sdsScene;
-    
-    
-    public MainApp() throws IOException
-    {
+    protected /*final*/ Scene sdsScene;
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        // This stuff *must* be done here, *not* in 'init' or in ctor,
+        // for execution on correct thread https://www.javaworld.com/article/3057072/
         Parent rootParent = FXMLLoader.load(this.getClass().getResource("/fxml/Scene.fxml"));
         this.rootScene = new Scene(rootParent);
         this.rootScene.getStylesheets().add("/styles/Styles.css");
-        
+
         Parent sdssParent = FXMLLoader.load(this.getClass().getResource("/fxml/SelectDataSource.fxml"));
         this.sdsScene = new Scene(sdssParent);
         this.rootScene.getStylesheets().add("/styles/Styles.css");
-    }
-    
-    
-    @Override
-    public void start(Stage stage) throws Exception {
-//        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
-//        
-//        Scene scene = new Scene(root);
-//        scene.getStylesheets().add("/styles/Styles.css");
-        
+
         stage.setTitle("txtshuffle");
         stage.setResizable(false);
 
